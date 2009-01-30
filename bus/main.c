a42d54b0294d0cb15ec70b20b3df16133dbf3509
Huang Peng <shawn.p.huang@gmail.com>
2009-01-30 16:19:48 +0800

    WIP.

--- bus/main.c
+++ bus/main.c
@@ -1,28 +1,50 @@
+/* vim:set et sts=4: */
+/* ibus - The Input Bus
+ * Copyright (C) 2008-2009 Huang Peng <shawn.p.huang@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include <stdlib.h>
 #include <locale.h>
 #include "server.h"
 
 static gboolean daemonize = FALSE;
+static gchar *desktop = "gnome";
 static gboolean verbose = FALSE;
 
 static const GOptionEntry entries[] = 
 {
     { "daemonize", 'd', 0, G_OPTION_ARG_NONE, &daemonize, "run ibus as background process", NULL },
+    { "desktop", 'n', 0, G_OPTION_ARG_STRING, &desktop, "specify the name of desktop session. [default=gnome]", "name" },
     { "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "verbose", NULL },
-	{ NULL },
+    { NULL },
 };
 
 
 gint
 main (gint argc, gchar **argv)
 {
-	GOptionContext *context;
-	BusServer *server;
-	GError *error = NULL;
+    GOptionContext *context;
+    BusServer *server;
+    GError *error = NULL;
     
     setlocale (LC_ALL, "");
-	
-	context = g_option_context_new ("- ibus daemon");
+    
+    context = g_option_context_new ("- ibus daemon");
     
     g_option_context_add_main_entries (context, entries, "ibus-daemon");
 
@@ -31,11 +53,11 @@ main (gint argc, gchar **argv)
         exit (-1);
     }
 
-	g_type_init ();
-	
-	server = bus_server_get_default ();
-	bus_server_listen (server);
-	bus_server_run (server);
+    g_type_init ();
+    
+    server = bus_server_get_default ();
+    bus_server_listen (server);
+    bus_server_run (server);
 
-	return 0;
+    return 0;
 }
